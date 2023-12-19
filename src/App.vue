31fd7f2be79e120644f76affff3f08560d851e79
Kaung Shan Oo <kaung669so@prestigein.com>
2023-12-19 11:53:36 +0630

    added Displays

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header/>
+  <div class="container">
+    <Balance/>
+    <IncomeExpenses/>
+    <TransactionList/>
+    <AddTransaction/>
+  </div>
+</template>
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import TransactionList from './components/TransactionList.vue';
+</script>
+<style>
+  
+</style>
